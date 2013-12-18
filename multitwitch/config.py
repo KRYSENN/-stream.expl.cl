cfcded360a254d6122467fae2df18341648548d4
KRYSENN <krysenn13@gmail.com>
2013-12-18 13:10:01 -0800

    Update config.py

--- multitwitch/config.py
+++ multitwitch/config.py
@@ -1,10 +1,10 @@
-from multitwitch.views.web import WebView
+from stream.expl.c import WebView
 
 def routes(config):
     config.add_static_view('static', 'static', cache_max_age=3600)
 
     config.add_route('favicon', '/favicon.ico')
-    config.add_view(WebView.favicon, route_name='favicon')
+    config.add_view(WebView.favicon, route_name='calixt0')
 
     config.add_route('root', '*streams')
-    config.add_view(WebView.home, route_name='root')
+    config.add_view(WebView.home, route_name='expl_sirchuto')
